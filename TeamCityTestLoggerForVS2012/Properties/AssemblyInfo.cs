6c505cbae4ff97bb9f03f7509a6a5e9137574b57
Jake Ginnivan <jake.ginnivan@readify.net>
2013-05-23 22:41:51 +0800

    Added VSIX Installer Project

---
+++ TeamCityTestLoggerForVS2012/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TeamCity Test Logger for VS 2012")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jake Ginnivan")]
+[assembly: AssemblyProduct("TeamCity Test Logger for VS 2012")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("TeamCityTestLoggerForVS2012_IntegrationTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001009bbf165beca10b6831daed40182b6cb89d7478286093660b4bda7861960b29d1a7e6c1a9ae2e13d8d695bf99e8b517506950467bfb4d179f362ff6c20b75a596b0627d0aaa630ac86c0a0dbfb0e5801e718bd92547597e5e34a0a329e7d45f7d591056f8ee47dbfb7b851a7c1d1545beffb37f028032c8f6e0318e66ee65c4b1")]
+[assembly: InternalsVisibleTo("TeamCityTestLoggerForVS2012_UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001009bbf165beca10b6831daed40182b6cb89d7478286093660b4bda7861960b29d1a7e6c1a9ae2e13d8d695bf99e8b517506950467bfb4d179f362ff6c20b75a596b0627d0aaa630ac86c0a0dbfb0e5801e718bd92547597e5e34a0a329e7d45f7d591056f8ee47dbfb7b851a7c1d1545beffb37f028032c8f6e0318e66ee65c4b1")]
